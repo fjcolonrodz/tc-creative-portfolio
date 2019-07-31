e4b3ee2d753984d9081b64ec25eab5fcdd7b7fe6
Francisco J Colón Rodríguez <fjcr1018@gmail.com>
2019-07-31 11:54:15 -0400

    Display Ads completed.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <top-section></top-section>
+    <display-ads></display-ads>
   </div>
 </template>
 
 <script>
 import TopSection from './components/TopSection.vue'
+import DisplayAds from './components/DisplayAds.vue'
 
 export default {
   name: 'app',
   components: {
-    TopSection
+    TopSection, DisplayAds
   }
 }
 </script>
--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -1,24 +1,34 @@
 <template>
     <div id="top-section">
-        <img src="../assets/logo.png" alt="">
-        <img class="image" src="../assets/hero.jpg" alt="">
-        <img class="shape" src="../assets/shape.png" alt="">
-        <h1>Creative Portfolio</h1>
-        <p>
-            TravelClick's Creative team offers strategic designs <br>
-            for clients such as Google Display Ads that are <br>
-            templates, personalized, or custom designed.
-        </p>
-        <p>
-            The team also creates Social Videos for advertisement <br>
-            on Facebook, Instagram, YouTube, and Pinterest.
-        </p>
-        <div id="ctas">
-            <a class="button" href="">Display Ads</a>
-            <a class="button" href="">Social Videos</a>
-            <a class="button" href="">Case Studies</a>
+        <div id="background">
+            <img class="image" src="../assets/hero.jpg" alt="">
+            <img class="shape" src="../assets/shape.png" alt="">
         </div>
-        <img src="../assets/google-partner.png" alt="">
+
+        <div id="content">
+            <img class="logo" src="../assets/logo.png" alt="">
+
+            <h1>Creative Portfolio</h1>
+
+            <p>
+                TravelClick's Creative team offers strategic designs <br>
+                for clients such as Google Display Ads that are <br>
+                templates, personalized, or custom designed.
+            </p>
+            <p>
+                The team also creates Social Videos for advertisement <br>
+                on Facebook, Instagram, YouTube, and Pinterest.
+            </p>
+
+            <div id="ctas">
+                <a class="button" href="">Display Ads</a>
+                <a class="button" href="">Social Videos</a>
+                <a class="button" href="">Case Studies</a>
+            </div>
+
+            <img class="google_partner" src="../assets/google-partner.png" alt="">
+        </div>
+
     </div>
 </template>
 
@@ -29,28 +39,48 @@
 </script>
 
 <style scoped>
-    img {
-        width: 155px;
+    #background {
+        position:relative;
+        height: 650px;
+        overflow: hidden;
     }
-    h1 {
+    #background .image {
+        position: absolute;
+        top: -300px;
+        transform: scaleX(-1);
+        width: 100%;
+        z-index: -3;
+    }
+    #background .shape {
+        position: absolute;
+        left: -1175px;
+        opacity: .75;
+        top: -550px;
+        transform: rotate(-140deg);
+        width: 1920px;
+        z-index: -2;
+    }
+    #content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        padding: 4em 9em;
+    }
+    #content h1 {
         color: white;
+        font-family: Amadeus;
         font-size: 46px;
+        font-weight: normal;
     }
-    p {
+    #content p {
         color: white;
         font-size: 25px;
     }
-    #top-section {
-        height: 500px;
-        overflow: hidden;
-        padding: 3em 11em;
-        position: absolute;
-        width: 100%;
-    }
-    #ctas {
+    #content #ctas {
         margin: 3em 0;
     }
-    .button {
+    #content .button {
         border: 1px solid white;
         border-radius: .25em;
         color: white;
@@ -60,21 +90,11 @@
         padding: .5em 2em;
         text-decoration: none;
     }
-    .image {
-        position: absolute;
-        top: -475px;
-        left: -350px;
-        transform: scaleX(-1);
-        width: 100%;
-        z-index: -3;
+    #content .logo, #content .google_partner {
+        width: 150px;
     }
-    .shape {
-        position: absolute;
-        left: -1175px;
-        opacity: .75;
-        top: -575px;
-        transform: rotate(-140deg);
-        width: 1920px;
-        z-index: -2;
+    #top-section {
+        overflow: hidden;
+        position: relative;
     }
 </style>
